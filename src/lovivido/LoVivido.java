be205c03a23c5958cfc4220aa9bf73fbb9b70fbb
otorradomiguez <otorradomiguez@victoria2.danielcastelao.org>
2016-12-12 08:55:15 +0100

    Renombro las variables para dejar mas claro su significado

--- src/lovivido/LoVivido.java
+++ src/lovivido/LoVivido.java
@@ -9,30 +9,30 @@ import javax.swing.JOptionPane;
 public class LoVivido {
 
 public static void main(String[] args) {
-String n;
-String e;
+String nombre;
+String edad;
 
-int m;
-int d;
-int h;
-int a;
+int meses;
+int dias;
+int horas;
+int anhos;
 
-n = JOptionPane.showInputDialog ("Escriba su nombre: ");
-e = JOptionPane.showInputDialog ("Escriba su edad: ");
+nombre = JOptionPane.showInputDialog ("Escriba su nombre: ");
+edad = JOptionPane.showInputDialog ("Escriba su edad: ");
  
-a = Integer.parseInt (e);
+anhos = Integer.parseInt (edad);
  
-m = (a*12);
-d = (a*365);
-h = (d*24);
+meses = (anhos*12);
+dias = (anhos*365);
+horas = (dias*24);
  
- JOptionPane.showMessageDialog(null,  "Meses: "  +m,"Numero de meses vividos de " +n,JOptionPane.INFORMATION_MESSAGE);
+ JOptionPane.showMessageDialog(null,  "Meses: "  +meses,"Numero de meses vividos de " +nombre,JOptionPane.INFORMATION_MESSAGE);
 
 
- JOptionPane.showMessageDialog(null, "Días: " +d,"Numero de días vividos de "+n,JOptionPane.INFORMATION_MESSAGE);
+ JOptionPane.showMessageDialog(null, "Días: " +dias,"Numero de días vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
 
 
- JOptionPane.showMessageDialog(null, "Horas: " +h,"Numero de horas vividos de "+n,JOptionPane.INFORMATION_MESSAGE);
+ JOptionPane.showMessageDialog(null, "Horas: " +horas,"Numero de horas vividos de "+nombre,JOptionPane.INFORMATION_MESSAGE);
 
    System.exit (0);
 
